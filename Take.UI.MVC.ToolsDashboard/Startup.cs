0b7f26b1ed3c62e21aefc91f88917a9d5fffb588
Pedro Andrade <pfandrade10@hotmail.com>
2021-08-09 23:09:54 -0300

    ajuste

--- Take.UI.MVC.ToolsDashboard/Startup.cs
+++ Take.UI.MVC.ToolsDashboard/Startup.cs
@@ -113,6 +113,7 @@ namespace Take.UI.MVC.ToolsDashboard
             }
             else
             {
+                //app.UseExceptionHandler("/Home/Error");
                 app.UseDeveloperExceptionPage();
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 //app.UseHsts();
